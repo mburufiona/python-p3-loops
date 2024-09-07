8574b508d156673a1df14c1f9387b712783b6e95
fiona <fiona.mburu@student.moringaschool.com>
2024-09-07 04:49:12 +0300

    "Implemented happy_new_year, square_integers, and fizzbuzz functions in looping.py"

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    for x in range(10, 0, -1):
+        print(x)
+        print("Happy New Year!")
+        
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = [x * x for x in int_list]
+    return square_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+
+    
